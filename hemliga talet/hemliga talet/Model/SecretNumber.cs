5ad8cc6053911f34ce304748b0a8f36f3d982e6e
ej222pj <ej222pj@student.lnu.se>
2014-02-03 19:32:01 +0100

    Klar med cs filen, om det funkar

--- hemliga talet/hemliga talet/Model/SecretNumber.cs
+++ hemliga talet/hemliga talet/Model/SecretNumber.cs
@@ -5,34 +5,59 @@ using System.Web;
 
 namespace hemliga_talet.Model
 {
-    enum Outcome { Indefine, Low, High, Correct, NoMoreGuesses, PreviousGuess }
+    public enum Outcome { Indefine, Low, High, Correct, NoMoreGuesses, PreviousGuess }
     public class SecretNumber
     {
 
         private int _number;
-        private List<int> _previousGuesses;
+        private List<int> _previousGuesses = new List<int>();
+        public const int MaxNumberOfGuesses = 7;
 
-        const int MaxNuumberOfGuesses = 7;
-
-        public bool CanMakeGuess
-        {
-            private get; 
-        }
-        public int Count
-        {
-            private get;
-        }
+        public bool CanMakeGuess { get; set; }
+        public int Count { get; set; }
         public int? Number
         {
-            private get;
+            get { return _number; }
         }
-        public Outcome Outcome
+        public Outcome Outcome { get; set; }
+        public IEnumerable<int> PreviousGuesses
         {
-            private get;
-            private set;
+            get { return _previousGuesses; }
         }
 
-
-
+        public void Initialize()
+        {     
+            Count = 0;
+            Random randomNumber = new Random();
+            _number = randomNumber.Next(1, 101);
+            
+        }
+        public Outcome MakeGuess(int guess)
+        {
+            
+            if (guess < 1 || guess > 100 || Count > MaxNumberOfGuesses)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                if (guess > _number)
+                {
+                    return Outcome.High;
+                }
+                else if (guess < _number)
+                {
+                    return Outcome.Low;
+                }
+                else
+                {
+                    return Outcome.Correct;
+                }
+            }
+        }
+        public SecretNumber()
+        {
+            Initialize();
+        }
     }
 }
